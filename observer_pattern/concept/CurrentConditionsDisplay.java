be5e8ebd3b3b830c7199be4d934f14b408575969
woodsection <82582199+woodsection@users.noreply.github.com>
2022-06-09 06:47:19 +0900

    ADD observer pattern

---
+++ observer_pattern/concept/CurrentConditionsDisplay.java
@@ -0,0 +1,20 @@
+public class CurrentConditionsDisplay implements Observer, DisplayElement {
+    private float temperature;
+    private float humidity;
+    private WeatherData weatherData;
+
+    public CurrentConditionsDisplay(WeatherData weatherData) {
+        this.weatherData = weatherData;
+        weatherData.registerObserver(this);
+    }
+
+    public void update(float temperature, float humidity, float pressure) {
+        this.temperature = temperature;
+        this.humidity = humidity;
+        display();
+    }
+
+    public void display() {
+        System.out.println("온도: " + temperature + "F, 습도 : " + humidity + "%");
+    }
+}
